04b01f97a043f0658653cf9384f066cd78a7b823
akhil-tv <akhil@volyty.com>
2024-12-02 10:55:17 +0530

    modifications in the code base

--- src/Security/SecurityAuthenticator.php
+++ src/Security/SecurityAuthenticator.php
@@ -93,7 +93,7 @@ class SecurityAuthenticator extends AbstractFormLoginAuthenticator implements Pa
 //            return new RedirectResponse($targetPath);
 //        }
 
-        return new RedirectResponse($this->urlGenerator->generate('user_home_page'));
+        return new RedirectResponse($this->urlGenerator->generate('app_contacts_index'));
 //        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
